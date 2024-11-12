b4bcfaaf85b694cb1f5ae75cbc0ee465a60025bc
korxniowskih <hubkorz@student.agh.edu.pl>
2024-11-12 14:09:39 +0100

    12.11 wdi

--- wdi/listopad/zad8.py
+++ wdi/listopad/zad8.py
@@ -1,6 +1,8 @@
 import random   
 wymiar = int(input("podaj wymiar tablicy: "))
 tablica = []
+przykladowa_tablica_tak = [[27, 852, 485, 714], [323, 644, 940, 203], [777, 64, 249, 972], [572, 282, 510, 999]]
+przykladowa_tablica_nie = [[24, 852, 485, 714], [313, 644, 940, 203], [701, 64, 249, 972], [418, 282, 510, 999]]
 
 for x in range(wymiar):
     wiersz = []
@@ -10,15 +12,12 @@ for x in range(wymiar):
 
 def Zbudowana_z_pierwszych(x):
     pierwsze = [2,3,5,7]
-    x = str(x)
-    for i in x:
-        i = int(i)
-        if i not in pierwsze:
+    while x > 0:
+        if x % 10 not in pierwsze:
             return False
+        x = x // 10
     else:
         return True
-    
-print(tablica)
 
 def Czy_w_wierszu(tablica):
     for wiersze in tablica:
@@ -26,8 +25,18 @@ def Czy_w_wierszu(tablica):
             if Zbudowana_z_pierwszych(liczba):
                 break
         else:
-            return "nie każdy wiersz zawiera"
+            return "nie każdy wiersz zawiera \n"
     else:
-        return "każdy wiersz zawiera"
-            
+        return "każdy wiersz zawiera \n"
+
+for x in tablica:
+    print(x)
 print(Czy_w_wierszu(tablica))
+
+for i in przykladowa_tablica_nie:
+    print(i)
+print(Czy_w_wierszu(przykladowa_tablica_nie))
+    
+for j in przykladowa_tablica_tak:
+    print(j)  
+print(Czy_w_wierszu(przykladowa_tablica_tak))
